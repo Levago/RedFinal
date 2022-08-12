7d67b740b5ce58a3066771c3ad37602358cd5354
Levago <84531485+Levago@users.noreply.github.com>
2022-08-12 11:07:55 -0500

    Add files via upload

---
+++ build/precache-manifest.c0ac773f79b679be444af3a573c808cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3738be21ebba0eb251c4782cbb89030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84ccdd4fc79304210ba6",
+    "url": "/static/css/main.a0b4a17c.chunk.css"
+  },
+  {
+    "revision": "d79b369f3b0ca397eb5c",
+    "url": "/static/js/2.ee8a3179.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.ee8a3179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ccdd4fc79304210ba6",
+    "url": "/static/js/main.a91a2749.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "acc6e4360c30d640edf18454889da7a1",
+    "url": "/static/media/showcase.acc6e436.jpg"
+  },
+  {
+    "revision": "13c03d1cee8db68215f3446d8e9e7f6b",
+    "url": "/static/media/spinner.13c03d1c.gif"
+  }
+]);
