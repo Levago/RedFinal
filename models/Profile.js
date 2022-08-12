3be2e22e830975249d22591a7343cfc55917301e
Levago <84531485+Levago@users.noreply.github.com>
2022-08-12 10:58:16 -0500

    Add files via upload

---
+++ models/Profile.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    },
+    location: {
+        type: String,
+    },
+    bio: {
+        type: String,
+    },
+    social: {
+        youtube: {
+            type: String,
+        },
+        twitter: {
+            type: String,
+        },
+        facebook: {
+            type: String,
+        },
+        linkedin: {
+            type: String,
+        },
+        instagram: {
+            type: String,
+        },
+    },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
